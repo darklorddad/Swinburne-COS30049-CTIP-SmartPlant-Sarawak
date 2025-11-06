bee6ed26776adb4f0e5ad5bb292bb78d0b6d8b96
Uwe1209 <liewyouwee123@gmail.com>
2025-11-06 18:43:56 +0800

    update tab

--- SmartPlant/tabs/Tabs.js
+++ SmartPlant/tabs/Tabs.js
@@ -3,15 +3,53 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import DashboardScreen from "../src/screens/DashboardScreen";
 import MapScreen from "../src/screens/MapScreen";
 import HistoryScreen from "../src/screens/HistoryScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Dashboard" component={DashboardScreen} />
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="History" component={HistoryScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerTitleAlign: "center",
+        tabBarActiveTintColor: "#2ecc71",
+        tabBarInactiveTintColor: "#777",
+        tabBarStyle: { backgroundColor: "#fff", height: 60 },
+      }}
+    >
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardScreen}
+        options={{
+          headerShown: true,
+          headerTitle: "Dashboard", // top center title
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="speedometer-outline" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Map"
+        component={MapScreen}
+        options={{
+          headerTitle: "Map",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map-outline" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{
+          headerTitle: "History",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="analytics-outline" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
