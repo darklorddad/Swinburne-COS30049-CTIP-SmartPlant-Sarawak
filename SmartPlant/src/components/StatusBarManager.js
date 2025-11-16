21efd01542a9f9acc22ad0dd40b047ad6c22a185
darklorddad <danieltiong000@gmail.com>
2025-11-16 17:35:37 +0800

    fix: Reduce top padding constants for safe area

--- SmartPlant/src/components/StatusBarManager.js
+++ SmartPlant/src/components/StatusBarManager.js
@@ -1,4 +1,4 @@
 import { Platform, StatusBar } from 'react-native';
 
-export const TOP_PAD = Platform.OS === 'ios' ? 56 : (StatusBar.currentHeight || 0) + 8;
-export const EXTRA_TOP_SPACE = 18;
+export const TOP_PAD = Platform.OS === 'ios' ? 48 : (StatusBar.currentHeight || 0) + 4;
+export const EXTRA_TOP_SPACE = 12;
